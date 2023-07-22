c8c7a16834abf5d1c27b365e7faa8647e93f4b1f
KarlaMacedo <karlapaolamacedo@gmail.com>
2023-07-22 10:42:55 -0600

    arregla bug de navegación

--- src/components/pages/Home/home.jsx
+++ src/components/pages/Home/home.jsx
@@ -1,28 +1,22 @@
-/* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
-import './home.css';
-import logo from '../../../assets/Images/logo.png';
-import Button from '../../button/button.jsx';
-import { Redirect } from 'react-router-dom';
+import './home.css'
+import logo from '../../../assets/Images/logo.png'
+import Button from '../../button/button.jsx'
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
-  const [redirectToLogin, setRedirectToLogin] = useState(false);
+  const navigate = useNavigate();
 
   const handleClick = () => {
     console.log('Hola HOME');
-    setRedirectToLogin(true);
+    navigate('/login');
   };
-
-  if (redirectToLogin) {
-    return <Redirect to="/login" />;
-  }
 
   return (
     <>
       <div className='home'>
         <img src={logo} className="logo" alt="img logo" />
-        <Button label="INICIAR SESIÓN" onClick={handleClick} classButton="enter" />
+        <Button label="INICIAR SESIÓN" onClick={handleClick} classButton="enter"/>
       </div>
     </>
-  );
+  )
 }
