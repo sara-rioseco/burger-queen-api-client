fdd3381f28a9dbf4550453d2bb31f2585d180102
KarlaMacedo <karlapaolamacedo@gmail.com>
2023-07-22 10:28:26 -0600

    agrega footer, arregla css de home e intento de navegación por el router

--- src/components/pages/Home/home.jsx
+++ src/components/pages/Home/home.jsx
@@ -1,19 +1,28 @@
-import './home.css'
-import logo from '../../../assets/Images/logo.png'
-import Button from '../../button/button.jsx'
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import './home.css';
+import logo from '../../../assets/Images/logo.png';
+import Button from '../../button/button.jsx';
+import { Redirect } from 'react-router-dom';
 
 export default function Home() {
+  const [redirectToLogin, setRedirectToLogin] = useState(false);
 
   const handleClick = () => {
     console.log('Hola HOME');
+    setRedirectToLogin(true);
   };
+
+  if (redirectToLogin) {
+    return <Redirect to="/login" />;
+  }
 
   return (
     <>
       <div className='home'>
         <img src={logo} className="logo" alt="img logo" />
-        <Button label="HOME" onClick={handleClick} />
+        <Button label="INICIAR SESIÓN" onClick={handleClick} classButton="enter" />
       </div>
     </>
-  )
+  );
 }
