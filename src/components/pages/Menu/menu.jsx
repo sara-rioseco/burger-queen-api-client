92d33fff893c164c5c05a49863a6cb63169dec15
sara-rioseco <sara.rioseco@gmail.com>
2023-07-25 17:46:29 -0400

    adds services file for common fx

--- src/components/pages/Menu/menu.jsx
+++ src/components/pages/Menu/menu.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 // CSS
 import './menu.css'
 // COMPONENTS
 import logo from '../../../assets/Images/logo.png'
 import Button from '../../button/button.jsx'
 import { menuLogic } from '../../../utils/menu.jsx'
+import { Services } from '../../../utils/services.jsx';
 // ASSETS
 import Out from '../../../assets/Images/out.png'
 import Edit from '../../../assets/Images/editar.png'
@@ -13,18 +15,30 @@ import Delete from '../../../assets/Images/borrar.png'
 import Check from '../../../assets/Images/listo.png'
 
 export default function Menu() {
-  const { getProductImg} = menuLogic();
+  const {
+    getTokenAdmin,
+    getTokenWaiter,
+    getTokenChef
+  } = Services();
 
-  const [showMenu, setShowMenu] = useState('Desayuno');
+  const { getProductImg } = menuLogic();
+
+  const [showMenu, setShowMenu] = useState(false);
+
   const handleClick = () => {
-    console.log('Hola menú');
+    getTokenWaiter()
   };
 
   return (
     <>
       <div className='container'>
-        <Button label='DESAYUNO' onClick={handleClick} classButton='buttonMenu'/> 
-        <Button label='ALMUERZO Y CENA' onClick={handleClick} classButton='buttonMenu'/>
+        <nav className='nav-bar'>
+          <Button label='DESAYUNO' onClick={handleClick} classButton='buttonMenu'/> 
+          <Button label='ALMUERZO Y CENA' onClick={handleClick} classButton='buttonMenu'/>
+        </nav>
+        <div className='products-grid'>
+
+        </div>
       </div>
     </>
   )
