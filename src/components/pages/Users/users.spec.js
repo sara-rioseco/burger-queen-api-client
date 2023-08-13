6a563c198cee11ab8b6ca5ef497342d936855537
KarlaMacedo <karlapaolamacedo@gmail.com>
2023-08-13 10:30:36 -0600

    test boton chef y mesero de users

--- src/components/pages/Users/users.spec.js
+++ src/components/pages/Users/users.spec.js
@@ -9,11 +9,11 @@ import MockAdapter from 'axios-mock-adapter';
 import { UsersLogic } from '../../../utils/users.jsx';
 import { useNavigate as useNavigateMock } from 'react-router-dom'; // navegar entre router
 
-// // Reemplaza la importación original de useNavigate con la función simulada
-// jest.mock('react-router-dom', () => ({
-//     ...jest.requireActual('react-router-dom'), // Mantener las importaciones reales
-//     useNavigate: jest.fn(), // Mockear useNavigate
-// }));
+// Reemplaza la importación original de useNavigate con la función simulada
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'), // Mantener las importaciones reales
+    useNavigate: jest.fn(), // Mockear useNavigate
+}));
 
 
 describe.only('Componente Users', () => {
@@ -21,38 +21,38 @@ describe.only('Componente Users', () => {
         jest.clearAllMocks();
     });
 
-    it('Se renderiza sin errores', () => {
-        render(
-            <MemoryRouter>
-                <Users />
-            </MemoryRouter>
-        );
+    // it('Se renderiza sin errores', () => {
+    //     render(
+    //         <MemoryRouter>
+    //             <Users />
+    //         </MemoryRouter>
+    //     );
 
-        const titleElement = screen.getByText('Usuarios');
-        expect(titleElement).toBeInTheDocument();
+    //     const titleElement = screen.getByText('Usuarios');
+    //     expect(titleElement).toBeInTheDocument();
 
-        const filterLabel = screen.getByText('Filtrar usuarios por puesto :');
-        expect(filterLabel).toBeInTheDocument();
+    //     const filterLabel = screen.getByText('Filtrar usuarios por puesto :');
+    //     expect(filterLabel).toBeInTheDocument();
 
-        const administradorCheckbox = screen.getByLabelText('Administrador');
-        const meseroCheckbox = screen.getByLabelText('Mesero');
-        const chefCheckbox = screen.getByLabelText('Cocinero');
-        expect(administradorCheckbox).toBeInTheDocument();
-        expect(meseroCheckbox).toBeInTheDocument();
-        expect(chefCheckbox).toBeInTheDocument();
+    //     const administradorCheckbox = screen.getByLabelText('Administrador');
+    //     const meseroCheckbox = screen.getByLabelText('Mesero');
+    //     const chefCheckbox = screen.getByLabelText('Cocinero');
+    //     expect(administradorCheckbox).toBeInTheDocument();
+    //     expect(meseroCheckbox).toBeInTheDocument();
+    //     expect(chefCheckbox).toBeInTheDocument();
 
-        const AddButton = screen.getByAltText('buttonAddUsers');
-        expect(AddButton).toBeInTheDocument();
+    //     const AddButton = screen.getByAltText('buttonAddUsers');
+    //     expect(AddButton).toBeInTheDocument();
 
-        const logoutButton = screen.getByAltText('Cerrar sesión');
-        expect(logoutButton).toBeInTheDocument();
+    //     const logoutButton = screen.getByAltText('Cerrar sesión');
+    //     expect(logoutButton).toBeInTheDocument();
 
-        const kitchenButton = screen.getByAltText('Ir a vista de cocina');
-        expect(kitchenButton).toBeInTheDocument();
+    //     const kitchenButton = screen.getByAltText('Ir a vista de cocina');
+    //     expect(kitchenButton).toBeInTheDocument();
 
-        const orderButton = screen.getByAltText('Ir a vista de pedidos');
-        expect(orderButton).toBeInTheDocument();
-    });
+    //     const orderButton = screen.getByAltText('Ir a vista de pedidos');
+    //     expect(orderButton).toBeInTheDocument();
+    // });
 
     // it('Debería renderizar la tabla de productos', () => {
     //     render(<MemoryRouter>
@@ -63,27 +63,75 @@ describe.only('Componente Users', () => {
     //     expect(screen.getAllByRole('columnheader')).toHaveLength(5);
     // });
 
-    // it('El boton de productos cambia a la ruta correcta', async () => {
-    //     // Crea una función simulada para useNavigate
-    //     const navigateMock = jest.fn();
-    //     useNavigateMock.mockImplementation(() => navigateMock);
+    it('El boton de productos cambia a la ruta correcta', async () => {
+        // Crea una función simulada para useNavigate
+        const navigateMock = jest.fn();
+        useNavigateMock.mockImplementation(() => navigateMock);
 
-    //     // Renderiza el componente Login
-    //     render(
-    //         <MemoryRouter>
-    //             <Users />
-    //         </MemoryRouter>
-    //     );
+        // Renderiza el componente Login
+        render(
+            <MemoryRouter>
+                <Users />
+            </MemoryRouter>
+        );
 
-    //     // Obtén los elementos necesarios
-    //     const productsButton = screen.getByText('Productos');
+        // Obtén los elementos necesarios
+        const productsButton = screen.getByText('Productos');
 
-    //     // Ejecuta la acción (click en el botón)
-    //     fireEvent.click(productsButton);
+        // Ejecuta la acción (click en el botón)
+        fireEvent.click(productsButton);
 
-    //     // Espera a que se complete la acción asíncrona
-    //     await waitFor(() => {
-    //         expect(navigateMock).toHaveBeenCalledWith('/products');
-    //     });
-    // });
+        // Espera a que se complete la acción asíncrona
+        await waitFor(() => {
+            expect(navigateMock).toHaveBeenCalledWith('/products');
+        });
+    });
+
+    it('El boton de chef cambia a la ruta correcta', async () => {
+        // Crea una función simulada para useNavigate
+        const navigateMock = jest.fn();
+        useNavigateMock.mockImplementation(() => navigateMock);
+
+        // Renderiza el componente Login
+        render(
+            <MemoryRouter>
+                <Users />
+            </MemoryRouter>
+        );
+
+        // Obtén los elementos necesarios
+        const kitchenButton = screen.getByAltText('Ir a vista de cocina');
+
+        // Ejecuta la acción (click en el botón)
+        fireEvent.click(kitchenButton);
+
+        // Espera a que se complete la acción asíncrona
+        await waitFor(() => {
+            expect(navigateMock).toHaveBeenCalledWith('/kitchen');
+        });
+    });
+
+    it('El boton de mesero cambia a la ruta correcta', async () => {
+        // Crea una función simulada para useNavigate
+        const navigateMock = jest.fn();
+        useNavigateMock.mockImplementation(() => navigateMock);
+
+        // Renderiza el componente Login
+        render(
+            <MemoryRouter>
+                <Users />
+            </MemoryRouter>
+        );
+
+        // Obtén los elementos necesarios
+        const orderButton = screen.getByAltText('Ir a vista de pedidos');
+
+        // Ejecuta la acción (click en el botón)
+        fireEvent.click(orderButton);
+
+        // Espera a que se complete la acción asíncrona
+        await waitFor(() => {
+            expect(navigateMock).toHaveBeenCalledWith('/orders');
+        });
+    });
 });
