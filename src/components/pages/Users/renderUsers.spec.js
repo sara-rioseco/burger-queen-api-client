c14979997cd402ad912184d6673d0183966ed0a8
KarlaMacedo <karlapaolamacedo@gmail.com>
2023-08-14 10:24:30 -0600

    cambios

--- src/components/pages/Users/renderUsers.spec.js
+++ src/components/pages/Users/renderUsers.spec.js
@@ -9,6 +9,67 @@ import MockAdapter from 'axios-mock-adapter';
 import { UsersLogic } from '../../../utils/users.jsx';
 import { useNavigate as useNavigateMock } from 'react-router-dom'; // navegar entre router
 
+// jest.mock('../../../utils/users', () => {
+//     const originalModule = jest.requireActual('../../../utils/users');
+//     return {
+//         ...originalModule,
+//         useUsersLogic: jest.fn(() => ({
+//             usersData: [{
+//                 "email": "iamawaiter@mail.com",
+//                 "password": "123456",
+//                 "role": "waiter",
+//                 "id": 1,
+//             },
+//             {
+//                 "email": "iamachef@mail.com",
+//                 "password": "123456",
+//                 "role": "chef",
+//                 "id": 2,
+//             },
+//             {
+//                 "email": "iamaadmin@mail.com",
+//                 "password": "123456",
+//                 "role": "admin",
+//                 "id": 3,
+//             }],
+//             getRoleLabel: jest.fn((role) => {
+//                 switch (role) {
+//                     case 'admin':
+//                         return 'Administrador';
+//                     case 'waiter':
+//                         return 'Mesero';
+//                     case 'chef':
+//                         return 'Cocinero';
+//                     default:
+//                         return role;
+//                 }
+//             }),
+//             handleOpenModalDeleteUsers: jest.fn(),
+//             handleConfirmDeleteClickUsers: jest.fn(),
+//             handleConfirmEditClickUsers: jest.fn(),
+//             handleOpenEditModalUsers: jest.fn(),
+//             handleCloseModalUsers: jest.fn(),
+//             handleAddClick: jest.fn(),
+//             setNewUser: jest.fn(),
+//             handleInputChange: jest.fn(),
+//             handleConfirmAddClick: jest.fn(),
+//             handleRoleCheckboxChange: jest.fn(),
+//             modalUserId: 2,
+//             selectedRoles: ['admin', 'waiter', 'chef'],
+//             modalOpenDeleteUsers: true,
+//             modalOpenEditUsers: true,
+//             editingUserData: 3,
+//             addModalOpen: true,
+//             newUser: {
+//                 email: 'test@example.com',
+//                 password: '123456',
+//                 role: 'admin',
+//             },
+//             errorLabel: 'error',
+//             errorLabelEdit: 'errorEdit',
+//         }))
+//     }
+// });
 
 describe('Componente Users', () => {
     it('Se renderiza sin errores', () => {
@@ -52,4 +113,46 @@ describe('Componente Users', () => {
         expect(screen.getByRole('table')).toBeInTheDocument();
         expect(screen.getAllByRole('columnheader')).toHaveLength(5);
     });
+
+    it('Debería renderizar la tabla con la información de los usuarios', () => {
+        const fakeUserData = [
+            {
+                "email": "iamawaiter@mail.com",
+                "password": "123456",
+                "role": "waiter",
+                "id": 1,
+            },
+            {
+                "email": "iamachef@mail.com",
+                "password": "123456",
+                "role": "chef",
+                "id": 2,
+            },
+            {
+                "email": "iamaadmin@mail.com",
+                "password": "123456",
+                "role": "admin",
+                "id": 3,
+            }
+        ];
+
+        // // Simula la lógica de UsersLogic con datos ficticios
+        // jest.mock('../../../utils/users.jsx', () => ({
+        //     ...jest.requireActual('../../../utils/users.jsx'),
+        //     useUsersLogic: jest.fn(() => ({
+        //         usersData: fakeUserData,
+        //     })),
+        // }));
+
+        render(
+            <MemoryRouter>
+                <Users />
+            </MemoryRouter>
+        );
+
+        expect(screen.getByText('iamawaiter@mail.com')).toBeInTheDocument();
+        expect(screen.getByText('iamachef@mail.com')).toBeInTheDocument();
+        expect(screen.getByText('iamaadmin@mail.com')).toBeInTheDocument();
+    });
 });
+
